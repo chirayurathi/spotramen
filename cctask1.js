fa3ba1dc170e98bae667826342943e6b29c4fd0e
chirayurathi <chirayurathi4@gmail.com>
2020-04-24 18:24:58 +0530

    search updated

--- cctask1.js
+++ cctask1.js
@@ -23,7 +23,6 @@ const carousel=()=>{
     let count=1;
     var size=carImg.width();
     carSlide.css("transform","translateX("+(-size * count)+"px)");
-    console.log('dvbdj');
     next.click(()=>{
         if(count >= carImg.length-1) return;
         console.log("next");
@@ -86,12 +85,81 @@ httpRequest.open('GET',' https://chirayurathi.github.io/spotramenJson/db.json');
 var jsonData = []
 var hotContainer = document.querySelector(".hotspots-container");
 httpRequest.onload= ()=>{
+    loadCards("Brand");
+};
+httpRequest.send();
+const loadCards = (para)=>{
     jsonData = JSON.parse(httpRequest.responseText);
+    sortAlg(jsonData,0,jsonData.length-1,para);
+    hotContainer.innerHTML="";
     for(var i=0;i<jsonData.length;i++)
     makeCard(jsonData[i]);
 };
-httpRequest.send();
 const makeCard = (jsonEle)=>{
-    htmlString="<div class='flip-container'><div class='flip-card'><div class='flip-front'><p>"+jsonEle["Brand"]+"</p></div><div class='flip-back'><p class='sub-head'>variety</p><p>"+jsonEle["Variety"]+"</p><p class='sub-head'>style</p><p>"+jsonEle["Style"]+"</p><p class='sub-head'>location</p><p>"+jsonEle["Country"]+"</p><p class='rank'>Ranked "+jsonEle["TopTen"]+"</p></div></div></div>";
+    htmlString="<div class='flip-container'><div class='flip-card'><div class='flip-front'><p>"+jsonEle["Brand"]+"</p></div><div class='flip-back'><p class='sub-head'>variety</p><p class='variety'>"+jsonEle["Variety"]+"</p><p class='sub-head'>style</p><p>"+jsonEle["Style"]+"</p><p class='sub-head'>location</p><p>"+jsonEle["Country"]+"</p><p class='rank'>Ranked "+jsonEle["TopTen"]+"</p></div></div></div>";
     hotContainer.insertAdjacentHTML('beforeend',htmlString);
+};
+const partition =(data,low,high,para)=>{
+    var pivot = data[high][para];
+    var i = low-1;
+    for(var j=low;j<high;j++)
+    {
+        if(data[j][para]<pivot)
+        {
+            i+=1;
+            var temp = data[i];
+            data[i]=data[j];
+            data[j]=temp;
+        }
+    }
+    var temp = data[i+1];
+    data[i+1]=data[high];
+    data[high]=temp;
+    return(i+1);
+};
+const sortAlg = (data,low,high,para)=>{
+    if(low<high){
+        var pi=partition(data,low,high,para);
+        sortAlg(data,low,pi-1,para);
+        sortAlg(data,pi+1,high,para);
+    }
+};
+var dropbtnbool =false;
+$('.drop button').click(()=>{
+    if(dropbtnbool===false)
+    $('.drop ul').attr('id','drop-active');
+    else
+    $('.drop ul').attr('id','');
+    dropbtnbool = !dropbtnbool;
+});
+var droplist =document.querySelectorAll('.drop ul li');
+droplist[0].addEventListener('click',()=>{
+    console.log("1");
+    loadCards("Brand");
+});
+droplist[1].addEventListener('click',()=>{
+    console.log("2");
+    loadCards("Country");
+});
+droplist[2].addEventListener('click',()=>{
+    console.log("3");
+    loadCards("Stars");
+});
+// var sortOptions = document.querySelectorAll(".drop li");
+// for(var i=0;i<3;i++)
+//    { sortOptions[i].addEventListener("click", ()=>{
+//         console.log('nkbh');
+//         loadCards($(this).text());
+//     });
+// } 
+var inputVal = $('input[type="text"]')
+const searchlist = ()=>{
+    for(var i=0; i<jsonData.length; i++ ){
+        if(jsonData[i]["Brand"].toLowerCase().indexOf(inputVal.val().toLowerCase())>-1){
+            $('.flip-container').eq(i).css('display','inline-block');
+        }
+        else{
+            $('.flip-container').eq(i).css('display','none');
+        }
+    }
 };
